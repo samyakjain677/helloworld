2d6be91152f4cd54d001224f8b06ce7dadfac0cb
samyakjain677 <44441096+samyakjain677@users.noreply.github.com>
2018-11-26 16:08:13 +0530

    Dynamic Component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import Hello from "./Hello";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Hiration Interns</h1>
       <h2>Lets create some miracle!</h2>
-      <HelloWorld />
+      <Hello data="Hello" />
     </div>
   );
 }
-class HelloWorld extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
-}
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
